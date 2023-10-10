12059cad42f08c7aff89d317363da7a272354d08
s22021 <s22021@std.it-college.ac.jp>
2023-10-10 10:03:22 +0900

    11.6を書いた

--- AsyncSample/app/src/main/java/jp/ac/it_college/std/s22021/asyncsample/MainActivity.kt
+++ AsyncSample/app/src/main/java/jp/ac/it_college/std/s22021/asyncsample/MainActivity.kt
@@ -1,11 +1,96 @@
 package jp.ac.it_college.std.s22021.asyncsample
 
 import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import android.os.Bundle
+import androidx.annotation.UiThread
+import androidx.annotation.WorkerThread
+import androidx.recyclerview.widget.LinearLayoutManager
+import jp.ac.it_college.std.s22021.asyncsample.databinding.ActivityMainBinding
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val DEBUG_TAG = "AsyncSample"
+        private const val WEATHER_INFO_URL =
+            "https://api.openweathermap.org/data/2.5/weather?lang=ja"
+        private const val APP_ID = BuildConfig.APP_ID
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        binding.rvCityList.apply {
+            adapter = CityAdapter {
+                receiveWetherInfo(it.q)
+            }
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+
+    @UiThread
+    private fun receiveWetherInfo(q: String) {
+        val url = "$WEATHER_INFO_URL&q=$q&appid=$APP_ID"
+        val executorService = Executors.newSingleThreadExecutor()
+        val backgroundReceiver = WeatherInfoBackgroundReceiver(url)
+        val future = executorService.submit(backgroundReceiver)
+        val result = future.get()
+        showWeatherInfo(result)
+    }
+
+    @UiThread
+    private fun showWeatherInfo(result: String) {
+        val root = JSONObject(result)
+        val cityName = root.getString("name")
+        val coord = root.getJSONObject("coord")
+        val latitude = coord.getDouble("lat")
+        val longitude = coord.getDouble("lon")
+        val weatherArray = root.getJSONArray("weather")
+        val current = weatherArray.getJSONObject(0)
+        val weather = current.getString("description")
+        // 以下、表示処理
+        binding.tvWeatherTelop.text = getString(R.string.tv_telop, cityName)
+        binding.tvWeatherDesc.text = getString(
+            R.string.tv_desc, weather, latitude, longitude
+        )
+    }
+
+    private class WeatherInfoBackgroundReceiver(val urlString: String) : Callable<String> {
+        @WorkerThread
+        override fun call(): String {
+            val url = URL(urlString)
+            val con = url.openConnection() as HttpURLConnection
+            con.apply {
+                connectTimeout = 1000
+                readTimeout = 1000
+                requestMethod = "GET"
+            }
+            return try {
+                con.connect()
+                val result = con.inputStream.reader().readText()
+                /*
+                InputStream.reader() で、自動的に UTF-8 であるとして Byte を String へ
+                変換して読み取ってくれる、InputStreamReader を作ってくれる。
+                さらに、InputStreamReader.readText() を呼び出せば
+                BufferedReader を使って1行ずつ読み取って全て1つの文字列に組み立てる
+                面倒くさい工程をすべて肩代わりしてくれる。
+                 */
+                con.disconnect()
+                result
+            } catch (ex: SocketTimeoutException) {
+                Log.w(DEBUG_TAG, "通信タイムアウト", ex)
+                ""
+            }
+        }
     }
 }
